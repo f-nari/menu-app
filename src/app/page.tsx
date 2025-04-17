35aa89844d5bb89ab68f90e8e41bf6ba1d318db2
fuum <wkrh832+programing@gmail.com>
2025-04-17 23:58:05 +0900

    途中です

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,51 @@
 'use client'
 
 import { createClient } from "@/utils/supabase/client";
+export default function Home() {
 
-
-export default  function  Home()  {
-
-  const signOut = async() => {
+  const signOut = async () => {
     console.log('ログアウト処理をします');
     const supabase = createClient()
     const { data: { user } } = await supabase.auth.getUser()
-    console.log('ｋろえがユーザーです',user);
+    console.log('ｋろえがユーザーです', user);
     const { error } = await supabase.auth.signOut()
   }
   return (
-    <div className="">
-      main
+    <div className="w-full flex justify-center h-full">
+      <div className="w-11/12 flex flex-col  text-center bg-amber-500 ">
+        <div className="bg-amber-200 h-16 flex justify-center flex-col ">
+          <h1 className="text-3xl ">献立作成 ここにタイトルはいる</h1>
+        </div>
+        <div className="h-60 bg-red-400  flex flex-col">
+          <div>
+          <table className="border-collapse border border-gray-400 ...">
+            <thead>
+              <tr>
+                <th className="border border-gray-300 ...">State</th>
+                <th className="border border-gray-300 ...">City</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="border border-gray-300 ...">Indiana</td>
+                <td className="border border-gray-300 ...">Indianapolis</td>
+              </tr>
+              <tr>
+                <td className="border border-gray-300 ...">Ohio</td>
+                <td className="border border-gray-300 ...">Columbus</td>
+              </tr>
+              <tr>
+                <td className="border border-gray-300 ...">Michigan</td>
+                <td className="border border-gray-300 ...">Detroit</td>
+              </tr>
+            </tbody>
+          </table>
+          </div>
+        </div>
+        <div>
+          レシピゾーン
+        </div>
+      </div>
     </div>
   );
 }
