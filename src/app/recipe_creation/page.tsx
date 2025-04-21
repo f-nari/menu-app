2b73c0a42d0e12993da617bbaece5f9ba3bc4ed0
fuum <wkrh832+programing@gmail.com>
2025-04-22 00:42:00 +0900

    行が増えるボタン設置完了

--- src/app/recipe_creation/page.tsx
+++ src/app/recipe_creation/page.tsx
@@ -1,8 +1,30 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 import picture from '../../img/55006CFD-8C14-43E0-8D23-85391213A88A.jpg'
 import Image from 'next/image'
 
+type Ingredients = {
+  title: string;
+  quantity: number;
+  unit: string
+}
+
 const Recipe_Creation = () => {
+  const [ingredientsstate, setIngredientstate] = useState<Ingredients[]>([{ title: '', quantity: 0, unit: '' }])
+  const [memo, setMemo] = useState('')
+  const [recipename, setRecipeName] = useState('')
+
+  const changeEvent = (e: React.ChangeEvent<HTMLInputElement>, junre: string, index: number) => {
+    const data = [...ingredientsstate]
+    data[index]['title'] = e.target.value
+  }
+
+  const clickHandler = () => {
+    const update = [...ingredientsstate, { title: '', quantity: 0, unit: '' }]
+    setIngredientstate(update)
+  }
+
   return (
     <div className='flex justify-center  h-screen text-[#4a4a4a]'>
       {/* 詳細ゾーン */}
@@ -14,24 +36,29 @@ const Recipe_Creation = () => {
           </div>
           {/*説明ゾーン */}
           <div className='flex flex-col ml-3 '>
-            <input type='text' className='text-3xl bg-[#f8f6f1] rounded-sm' placeholder='ハンバーグ' />
-            <p>作成者 廣川郁也</p>
-            <p className='text-2xl font-bold mt-5'>材料</p>
-            <div className='flex '>
-              <label htmlFor="" className='mr-4'>材料名</label>
-              <input type="text" placeholder='ひき肉' className='bg-[#f8f6f1] rounded-sm' />
-              <label htmlFor="" className='mr-4 ml-4' >量</label>
-              <input type="text" placeholder='300' className='bg-[#f8f6f1] rounded-sm w-20' />
-              <label htmlFor="" className='mr-4 ml-4'>単位</label>
-              <input type="text" placeholder='g' className='bg-[#f8f6f1] rounded-sm w-20' />
-            </div>
+            <input type='text' className='text-3xl bg-[#f8f6f1] rounded-sm' onChange={(e) => setRecipeName(e.target.value)} placeholder='ハンバーグ' />
+            <input type="text" placeholder='廣川郁也' />
+            <p className='text-2xl font-bold mt-5 mb-3'>材料</p>
+            {ingredientsstate.map((ingredient, index) => (
+              <div className='flex' key={index}>
+                <label htmlFor="" className='mr-4'>材料名</label>
+                <input type="text" placeholder='ひき肉' className='bg-[#f8f6f1] rounded-sm' onChange={(e) => (changeEvent(e, 'title', index))} />
+                <label htmlFor="" className='mr-4 ml-4' >量</label>
+                <input type="text" placeholder='300' className='bg-[#f8f6f1] rounded-sm w-20' onChange={(e) => (changeEvent(e, 'quantity', index))} />
+                <label htmlFor="" className='mr-4 ml-4'>単位</label>
+                <input type="text" placeholder='g' className='bg-[#f8f6f1] rounded-sm w-20' onChange={(e) => (changeEvent(e, 'unit', index))} />
+              </div>
+            ))}
+            <button onClick={() => clickHandler()}>材料追加</button>
           </div>
         </div>
         {/*下ゾーン memozorn*/}
         <div className='flex flex-col grow mt-5 '>
           <h1 className='h-6'>memo</h1>
-          <input type="text" className='w-full  grow rounded-sm bg-[#f8f6f1] mb-2' placeholder='コツやポイント' />
+          <input type="text" className='w-full  grow rounded-sm bg-[#f8f6f1] mb-2' onChange={(e) => setMemo(e.target.value)} placeholder='コツやポイント' />
         </div>
+        <button className='border' type='submit' >レシピ保存</button>
+
       </div>
     </div>
   )
