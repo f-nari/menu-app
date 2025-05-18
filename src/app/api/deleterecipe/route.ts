b00a0f4f32c15626daab39f07eab28f599279fcd
fuum <wkrh832+programing@gmail.com>
2025-05-18 22:26:21 +0900

    Close #90

---
+++ src/app/api/deleterecipe/route.ts
@@ -0,0 +1,25 @@
+import { createClient } from "@/utils/supabase/server";
+
+export async function DELETE(req:Request){
+    const supabase = await  createClient()
+    const {recipeId} = await req.json()
+
+    const recipesDeleteResponse = await supabase
+            .from('recipes')
+            .delete()
+            .eq('id', recipeId)
+
+    const ingredientsDeleteResponse = await supabase
+            .from('ingredients')
+            .delete()
+            .eq('recipe_id', recipeId)
+
+    console.log(recipesDeleteResponse,ingredientsDeleteResponse);
+
+    return new Response(JSON.stringify({ success: true }), {
+        status: 200,
+        headers: { 'Content-Type': 'application/json' },
+      })
+    
+
+}
