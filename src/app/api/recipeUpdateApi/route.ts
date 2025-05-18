bf1efade3510f512b991cf3090565a6e642f747d
fuum <wkrh832+programing@gmail.com>
2025-05-18 09:56:01 +0900

    Close #84

--- src/app/api/recipeUpdateApi/route.ts
+++ src/app/api/recipeUpdateApi/route.ts
@@ -5,25 +5,11 @@ import { createClient } from "@/utils/supabase/server"
 export async function POST(req:Request){
     const supabase = await createClient()
     const {data} = await req.json()
-    console.log("apiに渡されたデータです",data);
-    // apiに渡されたデータです {
-    //     recipeId: '76',
-    //     ingredients: [
-    //       { id: 1, title: 'しめじ', quantity: 300, unit: 'g' },
-    //       { id: 2, title: ' カニカマ', quantity: 100, unit: 'g' }
-    //     ],
-    //     recipeDetail: [
-    //       {
-    //         recipeName: 'しめじとカニカマサラダ',
-    //         recipeImageFile: 'https://mihayudoygfiuzekgjfo.supabase.co/storage/v1/object/public/recipeimages/1747120827208_IMG_0782.JPG',
-    //         recipeMemo: 'しめじはレンチン！'
-    //       }
-    //     ]
-    //   }
+    // console.log("apiに渡されたデータです",data);
 
     console.log(data.recipeDetail[0].recipeMemo);
     
-    const { error } = await supabase
+    const { error:updateError } = await supabase
         .from('recipes')
         .update({
             'name':`${data.recipeDetail[0].recipeName}`,
@@ -31,14 +17,33 @@ export async function POST(req:Request){
         })
         .eq('id', data.recipeId)
     
-    if(error){    
-        console.error(error)
-        return new Response(JSON.stringify({ error: error.message }), {
+    if(updateError){    
+        console.error(updateError)
+        return new Response(JSON.stringify({ error: updateError.message }), {
           status: 500,
           headers: { 'Content-Type': 'application/json' },
         })
       }
+
+      //一度データ削除
+    const response = await supabase
+        .from('ingredients')
+        .delete()
+        .eq('recipe_id', data.recipeId)
+
+
+        // {recipe_id:  1,title: 'しめじ', quantity: 300, unit: 'g'}がほ(しい
+    const insertData = data.ingredients.map((ingredient)=>({
+        'recipe_id':data.recipeId,'title':ingredient.title,'quantity':ingredient.quantity,'unit':ingredient.unit}
+    ))
+
+    
         
+
+    // // そして登録
+    const { error:insertError } = await supabase
+        .from('ingredients')    
+        .insert(insertData)
 
     return new Response(JSON.stringify({ success: true }), {
             status: 200,
