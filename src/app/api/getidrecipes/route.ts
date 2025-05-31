37a66e3ec373dd82f173f29b9d99532a9950de89
fuum <wkrh832+programing@gmail.com>
2025-05-31 09:29:48 +0900

    Close #164

--- src/app/api/getidrecipes/route.ts
+++ src/app/api/getidrecipes/route.ts
@@ -1,14 +1,16 @@
 import { createClient } from "@/utils/supabase/server";
 import { NextResponse } from "next/server";
 
-export async function  GET(request:Request)  {
+export async function GET(request: Request) {
     const { searchParams } = new URL(request.url)
     const recipeId = searchParams.get('id')
     const supabase = await createClient()
-    const { data:recipeData, error:recipe_dataErrer } = await supabase
+
+
+    const { data: recipeData, error: recipeDataGetErrer } = await supabase
         .from('recipes')
         .select('*')
-        .eq('id',recipeId)
+        .eq('id', recipeId)
         .single()
     // {
     //     id: 80,
@@ -20,26 +22,42 @@ export async function  GET(request:Request)  {
     //     memo: 'よく焼く。たまに生でも良い',
     //     image_url: 'https://mihayudoygfiuzekgjfo.supabase.co/storage/v1/object/public/recipeimages/1747644845986_IMG_0782.JPG'
     //   }
-    
 
-    const {data:resIngredients,error:ingredientsDataError} = await supabase
-        .from('ingredients')
-        .select('*')
-        .eq('recipe_id',recipeId)
-
-    if(ingredientsDataError){
-        return NextResponse.json({error:ingredientsDataError.message},{status:500})
+    if (recipeDataGetErrer) {
+        console.error('レシピデータ取得の際にエラー', recipeDataGetErrer.message);
+        return NextResponse.json({ error: 'データの取得の際にエラーが発生しました' }, {
+            status: 500
+        })
     }
 
-    const ingredients =  resIngredients.map((resIngredient)=>(
-        {title:resIngredient.title,quantity:resIngredient.quantity,unit:resIngredient.unit}
+    const { data: resIngredients, error: ingredientsDataError } = await supabase
+        .from('ingredients')
+        .select('*')
+        .eq('recipe_id', recipeId)
+
+    if (ingredientsDataError) {
+        console.error('ingredients取得の際にエラー', ingredientsDataError.message);
+        return NextResponse.json({ error: ingredientsDataError.message }, {
+            status: 500
+        })
+    }
+
+    const ingredients = resIngredients.map((resIngredient) => (
+        { title: resIngredient.title, quantity: resIngredient.quantity, unit: resIngredient.unit }
     ))
 
-    const resSignedUrl = await supabase.storage.from('recipeimages').createSignedUrl(recipeData.image_url.replace('https://mihayudoygfiuzekgjfo.supabase.co/storage/v1/object/public/recipeimages/',''), 60 * 60 * 24)
-    const signedUrl = resSignedUrl['data']?.signedUrl
-    const recipeDatasWithiSignedUrl =  {...recipeData,signedUrl,recipeIngredients:ingredients}
-    console.log('クライアント側に送る前の最後のデータです',recipeDatasWithiSignedUrl);
-    
+
+    const { data: resSignedUrl, error: signedUrlEroor } = await supabase.storage.from('recipeimages').createSignedUrl(recipeData.image_url.replace('https://mihayudoygfiuzekgjfo.supabase.co/storage/v1/object/public/recipeimages/', ''), 60 * 60 * 24)
+
+    if (signedUrlEroor) {
+        console.error('署名付きURL作成時にエラーが発生', signedUrlEroor.message);
+        return NextResponse.json({ error: '画像の取得に失敗しました' }, {
+            status: 500
+        })
+    }
+
+    const signedUrl = resSignedUrl?.signedUrl
+    const recipeDatasWithiSignedUrl = { ...recipeData, signedUrl, recipeIngredients: ingredients }
 
     return NextResponse.json(recipeDatasWithiSignedUrl)
 }
--- src/app/api/recipeUpdateApi/route.ts
+++ src/app/api/recipeUpdateApi/route.ts
@@ -1,52 +1,69 @@
 'use server'
 
 import { createClient } from "@/utils/supabase/server"
+import { NextResponse } from "next/server"
 
-export async function POST(req:Request){
-    const supabase = await createClient()
-    const {data} = await req.json()
+export async function POST(req: Request) {
+  const supabase = await createClient()
+  let data
+  try {
+    data = await req.json()
+  } catch (error) {
+    console.log('jsonエラー', error);
+    return NextResponse.json({ error: 'jsonエラー' }, {
+      status: 400
+    })
+  }
 
-    const { error:updateError } = await supabase
-        .from('recipes')
-        .update({
-            'name':`${data.recipeData.recipeName}`,
-            'memo':`${data.recipeData.recipeMemo}`,
-        })
-        .eq('id', data.recipeId)
-    
-    if(updateError){    
-        console.error(updateError)
-        return new Response(JSON.stringify({ error: updateError.message }), {
-          status: 500,
-          headers: { 'Content-Type': 'application/json' },
-        })
-      }
+  const { error: updateError } = await supabase
+    .from('recipes')
+    .update({
+      'name': data.recipeData.recipeName,
+      'memo': data.recipeData.recipeMemo,
+    })
+    .eq('id', data.recipeId)
 
-      //一度データ削除
-    const response = await supabase
-        .from('ingredients')
-        .delete()
-        .eq('recipe_id', data.recipeId)
+  if (updateError) {
+    console.error('recipesにアップデート時にエラー', updateError.message)
+    return NextResponse.json({ error: 'レシピの更新に失敗' }, {
+      status: 500
+    })
+  }
 
-    const ingredients = data.recipeData.ingredients.map((ingredient)=>({
-        'recipe_id':data.recipeId,'title':ingredient.title,'quantity':ingredient.quantity,'unit':ingredient.unit}
-    ))
 
-    const { error:insertError } = await supabase
-        .from('ingredients')    
-        .insert(ingredients)
+  const { error: deleteError } = await supabase
+    .from('ingredients')
+    .delete()
+    .eq('recipe_id', data.recipeId)
 
-        if(insertError){    
-            console.error(insertError)
-            return new Response(JSON.stringify({ error: insertError.message }), {
-              status: 500,
-              headers: { 'Content-Type': 'application/json' },
-            })
-          }
+  if (deleteError) {
+    console.error('ingredientsテーブル削除時にエラー', deleteError.message);
+    return NextResponse.json({ error: 'データの削除に失敗' }, {
+      status: 500
+    })
+  }
 
-    return new Response(JSON.stringify({ success: true }), {
-            status: 200,
-            headers: { 'Content-Type': 'application/json' },
-          })
-    
+  const ingredients = data.recipeData.ingredients.map((ingredient) => ({
+    'recipe_id': data.recipeId, 'title': ingredient.title, 'quantity': ingredient.quantity, 'unit': ingredient.unit
+  }
+  ))
+
+  const { error: insertError } = await supabase
+    .from('ingredients')
+    .insert(ingredients)
+
+  if (insertError) {
+    console.error(insertError.message)
+
+
+    return NextResponse.json({ error: 'データの登録に失敗' }, {
+      status: 500
+    })
+  }
+
+
+  return NextResponse.json({ success: true }, {
+    status: 200
+  })
+
 }
